39deadb611edcc2269b0e0d2bb1e17fe02303f40
10946017 <10946017@ntub.edu.tw>
2023-05-27 17:11:43 +0800

    5/27網頁程式

---
+++ templates/js/grouping.js
@@ -0,0 +1,67 @@
+const chartData = {
+    labels: ["正面關鍵字", "負面關鍵字"],
+    data: [70, 30],
+    keyword:["餐點好吃","服務品質差"],
+  };
+  
+  const myChart = document.querySelector(".my-chart");
+  const ul = document.querySelector(".programming-stats .details ul");
+  
+  new Chart(myChart, {
+    type: "doughnut",
+    data: {
+      labels: chartData.labels,
+      keyword:chartData.keyword,
+      datasets: [
+        {
+          label:"占比",
+          data: chartData.data,
+        },
+      ],
+    },
+    options: {
+      borderWidth: 10,
+      borderRadius: 2,
+      hoverBorderWidth: 0,
+      plugins: {
+        legend: {
+          display: false,
+        },
+      },
+    },
+  });
+  
+  const populateUl = () => {
+    chartData.labels.forEach((l, i) => {
+      let li = document.createElement("li");
+      li.innerHTML = `${l}: <span class='percentage'>${chartData.data[i]}%</span>`;
+      ul.appendChild(li);
+    });
+  };
+  
+  populateUl();
+
+  /*----------login---------- */
+  function show_hide() {
+    var login = document.getElementById("container1");
+    var signup = document.getElementById("container2");
+    var copyright = document.getElementById("copyright");
+  
+    if (login.style.display === "none") {
+        login.style.display = "block";  //lonin出現
+        document.getElementById("username").value="";
+        document.getElementById("password").value="";
+        signup.style.display = "none";  //signup消失
+        copyright.style.margin = "200px 0px 0px 0px";
+    } else {
+        login.style.display = "none";   //login消失
+        signup.style.display = "block"; //signup出現
+        signup.style.visibility="visible";
+        copyright.style.margin = "200px 0px 0px 0px";
+     
+        document.getElementById("fullname").value="";
+        document.getElementById("username2").value="";
+        document.getElementById("password2").value="";
+        document.getElementById("comfirm_password").value="";
+    }
+}
