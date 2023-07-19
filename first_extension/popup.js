a04e74d7ef3f73aa000b151f1e59174cc74c0eda
itsnataliekkk <10946001@ntub.edu.tw>
2023-07-20 01:32:11 +0800

    Extension version2

--- first_extension/popup.js
+++ first_extension/popup.js
@@ -1,16 +1,15 @@
 
-  // 
-  document.addEventListener('DOMContentLoaded', function() {
-    const applyFilterButton = document.getElementById('applyFilter');
-  
-    // 监听“应用筛选”按钮点击事件
-    applyFilterButton.addEventListener('click', function() {
-      // 获取用户输入的筛选关键词
-      const filterKeyword = document.getElementById('filter').value;
-  
-      // 向当前激活的标签页发送筛选关键词
-      chrome.tabs.query({ active: true, currentWindow: true }, function(tabs) {
-        chrome.tabs.sendMessage(tabs[0].id, { type: 'applyFilter', keyword: filterKeyword });
-      });
-    });
+// 點擊"篩選評價"按鈕後，向content發送篩選評價的訊息
+document.getElementById('applyFilter').addEventListener('click', function() {
+  const keyword = document.getElementById('keyword').value;
+  chrome.tabs.query({ active: true, currentWindow: true }, function(tabs) {
+    chrome.tabs.sendMessage(tabs[0].id, { type: 'applyFilter', keyword: keyword });
   });
+});
+
+// 點擊"清除篩選"按鈕後，向content發送清除篩選的訊息
+document.getElementById('clearFilter').addEventListener('click', function() {
+  chrome.tabs.query({ active: true, currentWindow: true }, function(tabs) {
+    chrome.tabs.sendMessage(tabs[0].id, { type: 'clearFilter' });
+  });
+});
