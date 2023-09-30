b7576b7ec616b988f3e1996d7fca9241571f0a62
10946017 <10946017@ntub.edu.tw>
2023-09-30 23:06:53 +0800

    0930資料可以傳到後台 差顯示在畫面

--- dj/Gapp/views.py
+++ dj/Gapp/views.py
@@ -1,3 +1,4 @@
+
 import openpyxl
 from random import randint
 from time import sleep
@@ -14,33 +15,72 @@ from selenium.webdriver import Chrome
 import numpy as np
 import codecs
 from openpyxl import Workbook
-
-
-
+from django.http import JsonResponse
 
 def testA(request, place_name="None", formatted_address="None"):
+    response_data = {}  # 默认的空字典
+    print("AAA")
     print(request.GET)
     data = dj.objects.all()
     data1 = store.objects.all()
-    # data = dj.objects.filter(star = 5) #要用一個條件傳到這裡的r
-    if request.method == "GET" and request.GET.get("address") != None:
-        print(request.GET)
+    # print("aaa" + request.method + "bbb")
+    #if request.GET.get("currentText") != None and request.GET.get("currentText") != None :
+    if  request.method == "GET" and request.GET.get("currentText") != None and request.GET.get("currentText") != None :    
+        currentText = request.GET.get("currentText")
+        addressText = request.GET.get("address")
+    
+        if currentText is not None and addressText is not None:
+            try:
+                print("name" , currentText)
+                print("address" , addressText)
+                # 查询匹配的dj对象
+                #matched_dj = dj.objects.get(
+                #matched_store = store.objects.get(
+                matched_store = store.objects.filter(storename = currentText , address=addressText).first()
+                    #storeid =  "1001"
+                     #storeid__storename=currentText,
+                     #storeid__address=addressText
+                    #storename = currentText
+                    #"饌味香麵食館", 
+                    #currentText,
+                    #address="100台灣臺北市中正區濟南路二段6-1號"
+                    #"100台灣臺北市中正區濟南路二段6-1號" 
+                    #addressText
+                #)#.objects.get(address=addressText)
+                # print(store.DoesNotExist)
+                # if store.DoesNotExist:
+                #     return JsonResponse({'error': 'No matching record found in store'}, status=404)
+                # print('id',matched_store.storeid)
+                # for item in matched_store:
+                #     print(item)
+                #print("ccc" + aa.+ "222")
 
-    try:
-        matched_dj = dj.objects.filter(
-            storeid__storename=place_name,
-            storeid__address=formatted_address
-        ).first()
+                matched_dj = dj.objects.get(
+                    storeid =  matched_store.storeid
+                     #storeid__storename=currentText,
+                     #storeid__address=addressText
+                )
+                print("ddd" + matched_dj.username  + "222")
+                # 构建JSON响应数据
+                response_data = {
+                    'username': matched_dj.username,
+                    'msgtime': matched_dj.msgtime,
+                    'star': matched_dj.star,
+                    'comment': matched_dj.comment,
+                    'effflag': matched_dj.effflag
+                }
+                #print("111" + response_data + "222")
+                # 返回JSON响应
+                return JsonResponse(response_data)
 
-        if matched_dj:
-            print("hello")
-                # 匹配成功，可以在这里返回匹配的数据或渲染相应的模板
-            return render(request, 'test.html', {'item': matched_dj})
+            except dj.DoesNotExist:
+                # 如果没有匹配的记录，返回错误消息和HTTP状态码
+                return JsonResponse({'error': 'No matching record found'}, status=404)
+        else:
+            # 如果缺少必要的参数，返回错误消息和HTTP状态码
+            return JsonResponse({'error': 'Missing currentText or address parameter'}, status=400)
 
-    except dj.DoesNotExist:
-            pass
+    # 如果请求方法不是GET，返回错误响应和HTTP状态码
+    # return render(request, "test.html",locals())
+    return render(request, "test.html", {'item': response_data})
 
-        # 如果没有匹配或发生异常，可以返回一个适当的响应
-    return render(request, "test.html",locals())
-
- 
