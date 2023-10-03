cbbc00d2f0c33367f353ef3d4dbb2fd373207dc8
Bobo0419 <119567185+Bobo0419@users.noreply.github.com>
2023-10-03 21:58:30 +0800

    Revert "0930資料可以傳到後台 差顯示在畫面"

--- dj/Gapp/urls.py
+++ dj/Gapp/urls.py
@@ -3,6 +3,5 @@ from django.urls import path
 from Gapp import views
 
 urlpatterns = [
-    # path('testA/', views.testA, name='testA'),
-    path('testA/<str:place_name>/<str:formatted_address>/', views.testA, name='testA'),
+    path('testA/', views.testA, name='testA'),
 ]
