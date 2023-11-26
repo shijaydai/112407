7e6791db91dd15931d743fc287a2b76955ce7cf4
10946017 <10946017@ntub.edu.tw>
2023-11-27 06:34:02 +0800

    斷詞分析 詞性完成

--- 演算法/斷詞分析/word/segmentation.py
+++ 演算法/斷詞分析/word/segmentation.py
@@ -1,20 +1,23 @@
-import jieba
+import jieba.posseg as pseg
 import pandas as pd
 
 # 載入CSV文件
 file_path = 'word.csv'  # 將 'your_file.csv' 替換為你的檔案路徑
 df = pd.read_csv(file_path, encoding='cp950')
 
+# 定義斷詞函數，包含詞性
+def segment_text_with_pos(text):
+    words = pseg.cut(text)
+    segmented_text = []
+    for word, pos in words:
+        segmented_text.append(f'{word}({pos})')
+    return ' '.join(segmented_text)
 
-# 定義斷詞函數
-def segment_text(text):
-    seg_list = jieba.cut(text, cut_all=False)
-    return ' '.join(seg_list)
-
-# 對DataFrame的某一列進行斷詞
+# 對DataFrame的某一列進行斷詞和顯示詞性
 column_to_segment = 'comment'  # 將 'column_name' 替換為你想要斷詞的列名稱
-df['segmented_text'] = df[column_to_segment].apply(segment_text)
+df['segmented_text_with_pos'] = df[column_to_segment].apply(segment_text_with_pos)
 
 # 保存結果到新的CSV文件
-output_file_path = 'output_segmented.csv'
+output_file_path = 'output_segmented_with_pos.csv'
 df.to_csv(output_file_path, index=False, encoding='cp950')
+
